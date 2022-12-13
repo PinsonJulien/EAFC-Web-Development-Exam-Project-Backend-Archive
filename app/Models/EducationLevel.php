78a0bd9b3852c05d18523bb006a27bddb3d694f6
Julien Pinson <pinsonj01@gmail.com>
2022-12-13 22:25:39 +0100

    Included SoftDeletes on all models

--- app/Models/EducationLevel.php
+++ app/Models/EducationLevel.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EducationLevel extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
     ];
